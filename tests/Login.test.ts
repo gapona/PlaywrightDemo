a5381cf3222f708892b4d99728f959d829ffec40
Name Lastname <name.lastname@techtellent.io>
2025-03-05 22:41:21 +0200

    Screenshots update and cleanup

--- tests/Login.test.ts
+++ tests/Login.test.ts
@@ -1,6 +1,5 @@
 import {test, expect} from '@playwright/test';
 import {LoginPage} from '../pages/LoginPage';
-import {allure} from "allure-playwright";
 
 test('User can log in and see Deposit button', async ({page}) => {
     const loginPage = new LoginPage(page);
@@ -13,18 +12,11 @@ test('User can log in and see Deposit button', async ({page}) => {
 });
 
 test('Failed test', async ({page}) => {
-  const loginPage = new LoginPage(page);
+    const loginPage = new LoginPage(page);
 
-  await loginPage.goto();
-  await loginPage.login('andrii.h@primegroup.io', 'wrong password');
+    await loginPage.goto();
+    await loginPage.login('andrii.h@primegroup.io', 'wrong password');
 
-  // Assert that the "Deposit" button is visible
-  await expect(page.getByRole('button', {name: 'Deposit', exact: true})).toBeVisible();
-});
-
-test.afterEach(async ({ page }, testInfo) => {
-  if (testInfo.status !== 'passed') { // Если тест упал
-    const screenshotPath = await page.screenshot();
-    allure.attachment('Failure Screenshot', screenshotPath, 'image/png');
-  }
+    // Assert that the "Deposit" button is visible
+    await expect(page.getByRole('button', {name: 'Deposit', exact: true})).toBeVisible();
 });
