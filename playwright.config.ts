aa34f1db8a7e7e66a99ee341570a963504f13570
andriihapon <andrii.hapon@namecheap.com>
2026-02-08 01:45:54 +0200

    api config update

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,23 +1,10 @@
-import { defineConfig } from '@playwright/test';
-import { desktopProjects } from './configs/desktop.config';
+import {desktopProjects} from "./configs/desktop.config";
+import {defineConfig} from "@playwright/test";
 
 export default defineConfig({
     testDir: './tests',
-    fullyParallel: true,
+    reporter: [['html', { open: 'never' }]],
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 2 : undefined,
-
-    reporter: [
-        ['html', { open: 'never' }]
-    ],
-
-    use: {
-        headless: !!process.env.CI,
-        trace: 'on-first-retry',
-        screenshot: 'only-on-failure',
-    },
-
-    projects: [
-        ...desktopProjects
-    ],
+    projects: [...desktopProjects],
 });
--- tests/api/config/api.config.ts
+++ tests/api/config/api.config.ts
@@ -1,5 +1,11 @@
 export const API_CONFIG = {
     baseUrl: 'https://fakerestapi.azurewebsites.net',
+
+    headers: {
+        contentType: 'application/json',
+        accept: 'text/plain',
+    },
+
     endpoints: {
         authors: '/api/v1/Authors',
         authorById: (id: number) => `/api/v1/Authors/${id}`,
@@ -7,9 +13,4 @@ export const API_CONFIG = {
         books: '/api/v1/Books',
         bookById: (id: number) => `/api/v1/Books/${id}`,
     },
-    headers: {
-        contentType: 'application/json',
-        accept: 'text/plain',
-    },
-    timeout: 30000,
 } as const;
